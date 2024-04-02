bc046d2ff1a8d98f12a48891bb0a49efd5473da8
Manuchexra <135632769+Manuchexra@users.noreply.github.com>
2024-04-02 07:00:33 +0500

    register

--- app/models.py
+++ app/models.py
@@ -4,9 +4,9 @@ class CustomUser(models.Model):
     username=models.CharField(max_length=200)
     password=models.CharField(max_length=20)
     email = models.EmailField(unique=True)
+    age = models.IntegerField()
     country = models.CharField(max_length=100, null=True, blank=True)
     first_name = models.CharField(max_length=100)
-    age = models.IntegerField()
     last_name = models.CharField(max_length=100)
     def __str__(self):
         return self.username
--- app/serializers.py
+++ app/serializers.py
@@ -1,13 +1,10 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
-
+from .models import CustomUser
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = CustomUser
         fields = ['username', 'password', 'email', 'age', 'country', 'first_name', 'last_name']
         extra_kwargs = {'password': {'write_only': True}}
 
-    def create(self, validated_data):
-        User = get_user_model()
-        user = User.objects.create_user(**validated_data)
-        return user
+    
